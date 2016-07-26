3b3a51c3fac8ab29e35819e42dc544d492e06754
master00041 <master041amin@gmail.com>
2016-07-26 15:20:47 +0430

    Create amin.lua

---
+++ plugins/amin.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+local text = 'هن'
+if matches[1] == 'amin' or 'امین then
+    if is_sudo(msg) then
+reply_msg(reply_id, text, ok_cb, false)
+end
+end 
+end
+return {
+patterns = {
+"^amin$",
+"^امین$",
+},
+run = run
+}
+
+end
