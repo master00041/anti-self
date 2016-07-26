bc94834bec692f1f8e0ba65fa92aa4ff315c67c3
master00041 <master041amin@gmail.com>
2016-07-26 15:26:14 +0430

    Create lockemoji.lua

---
+++ plugins/lockemoji.lua
@@ -0,0 +1,56 @@
+local function run(msg)
+
+    local data = load_data(_config.moderation.data)
+
+     if data[tostring(msg.to.id)]['settings']['lock_emoji'] == 'yes' then
+
+
+if msg.to.type == 'channel' and not is_momod(msg) then
+	delete_msg(msg.id,ok_cb,false)
+	else
+	kick_user(msg.from.id, msg.to.id)
+
+        return 
+      end
+   end
+end
+
+return {patterns = {
+    "😀",
+    "😆",
+    "😂",
+    "😘",
+    "❤️",
+    "😍",
+    "😊",
+    "💋",
+    "😭",
+    "😄",
+    "😔",
+    "☺️",
+    "👍🏻",
+    "😁",
+    "😒",
+    "😳",
+    "😜",
+    "🙈",
+    "😉",
+    "😃",
+    "😢",
+    "😚",
+    "😅",
+    "😞",
+    "😏",
+    "😡",
+    "😱",
+    "😝",
+    "🙊",
+    "😌",
+    "😋",
+    "👌🏻",
+    "😐",
+    "😕"
+}, run = run}
+
+--By @alireza_PT
+--channel : @create_antispam_bot
