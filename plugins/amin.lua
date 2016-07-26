7d4c008b62d0cc9b1134bbbdc7a321dd44560d1c
master00041 <master041amin@gmail.com>
2016-07-26 16:01:44 +0430

    Update amin.lua

--- plugins/amin.lua
+++ plugins/amin.lua
@@ -3,7 +3,7 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 local text = 'هن'
-if matches[1] == 'amin' or 'امین then
+if matches[1] == 'amin' or 'امین' then
     if is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
 end
