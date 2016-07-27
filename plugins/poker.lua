3e791a9916d1a3cb26658a3eaa0d221b2aa618a5
master00041 <master041amin@gmail.com>
2016-07-27 23:33:48 +0430

    Update poker.lua

--- plugins/poker.lua
+++ plugins/poker.lua
@@ -2,7 +2,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-local text = 'باز پوکر؟'
+local text = 'باز پوکر؟' or 'میگم پوکر نده عمه خر' or 'پوکر نده عمه سگ عه' or 'شیرم دهن اونی که پوکر ساخت'
 if matches[1] == '😐' then
     if not is_sudo(msg) then
 reply_msg(reply_id, text, ok_cb, false)
