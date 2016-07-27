01bfc8ca0df0f245c1064b822ca5b3fa2916a19c
master00041 <master041amin@gmail.com>
2016-07-27 23:57:18 +0430

    Update lockemoji.lua

--- plugins/lockemoji.lua
+++ plugins/lockemoji.lua
@@ -2,7 +2,7 @@ local function run(msg)
 
     local data = load_data(_config.moderation.data)
 
-     if data[tostring(msg.to.id)]['settings']['lock_emoji'] == 'yes' then
+     if data[tostring](msg.to.id)['settings']['lock_emoji'] == 'yes' then
 
 
 if msg.to.type == 'channel' and not is_momod(msg) then
